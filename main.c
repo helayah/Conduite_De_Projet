14e13bee7de75d0dcb6a50d0faa78e946af857c6
yazidlefoot <yazid_lefoot@hotmail.fr>
2016-03-09 13:30:56 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,75 +1,175 @@
 #include "interface.h"
+#include "parser.h"
 #include "objs.h"
-//#include "parser.h"
+#include "draw.h"
+
+#define EXT_OSM_LENGTH 4
 
 SDL_Window* pWindow = NULL;
 SDL_Renderer* pRenderer = NULL;
-node_t* array = NULL;
-//SDL_Surface *screen = NULL;
+root* Root = NULL;
 
-//GCC : gcc -Wall main.c interface.c -I./usr/x86_64-w64-mingw32/sys-root/mingw/include/SDL2 -L./usr/x86_64-w64-mingw32/sys-root/mingw/lib
-//GCC : gcc -Wall main.c parser.c -I/usr/include/libxml2 -lxml2 -o main
-int main(int argc, char **argv)
+int is_mtrFile(char *file_name);
+
+int main(int argc, char** argv)
 {
-	/*char* file;
-
-	file = "01_denver_lafayette.osm";
-	
-	if (parse(file) == FAILURE)
+	if(argc <= 1 || argc > 2)
 	{
-		fprintf(stderr, "%s\n", PARSING_FAILURE);
+		fprintf(stdout, "%s\n", NUMBER_ARGUMENT_FAILURE);
 		exit(EXIT_FAILURE);
-	}*/
-	array = malloc(sizeof(struct node_t *) * 2);
-    array[0]->id = 1;
-    array[0]->pos.longitute = 20.0;
-    array[0]->pos.latitude = 20.0;
-    
-    array[1]->id = 2;
-    array[1]->pos.longitute = 21.0;
-    array[1]->pos.latitude = 21.0;
-    
-    
-	SDL_Init(SDL_INIT_VIDEO);
+	}
+	
+	if(!is_mtrFile(argv[1]))
+	{
+		fprintf(stdout, "%s\n", NOT_AN_OS_FILE);
+		exit(EXIT_FAILURE);
+	}
+
+	if((Root = parse(argv[1])) == NULL)
+	{
+		fprintf(stdout, "%s\n", PARSING_FAILURE);
+		exit(EXIT_FAILURE);
+	}
+
+
+	if(Root != NULL)
+	{
+	
+		if (SDL_VideoInit(NULL) < 0)
+		{
+			fprintf(stdout, "%s\n", SDL_GetError());
+			Root_Free(Root);
+			exit(EXIT_FAILURE);
+		}
+	
+		pWindow = SDL_CreateWindow("SDL",
+
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WIDTH,
+		HEIGHT,
+		SDL_WINDOW_RESIZABLE);
+
+
+		if (pWindow == NULL)
+		{
+			fprintf(stdout, "%s\n", SDL_GetError());
+			Root_Free(Root);
+			SDL_Quit();
+			exit(EXIT_FAILURE);
+		}
+
+		pRenderer = SDL_CreateRenderer(
+			pWindow,
+			DEFAULT_INDEX,
+			SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+
+		if (pRenderer == NULL)
+		{
+			fprintf(stdout, "%s\n", SDL_GetError());
+			Root_Free(Root);
+			SDL_DestroyWindow(pWindow);
+			SDL_Quit();
+			exit(EXIT_FAILURE);
+		}
+
+		SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
+		SDL_RenderClear(pRenderer);
+
+
+		SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 20);
+		SDL_RenderPresent(pRenderer);
+
+		draw(Root);
+		
+		rest();
+	
+		SDL_DestroyRenderer(pRenderer);
+		SDL_DestroyWindow(pWindow);
+		SDL_Quit();
+
+		Root_Free(Root);	
+	}
+	
+	/*int size_node = 2, size_way = 1;
+
+	if (SDL_VideoInit(NULL) < 0)
+	{
+		fprintf(stdout, "%s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	bound.minlat = 48.8542200;
+	bound.minlon = 2.3625300;
+	bound.maxlat = 48.8569130;
+	bound.maxlon = 2.3688710;
+
+	node = Root_Init(size_node, size_way);
+
+	node->arrayNode[0] =  Node_Init(100, 20, 20);
+	node->arrayNode[1] = Node_Init(100, 21, 21);
+	way = Ways_Init(1, 2);
 
 	pWindow = SDL_CreateWindow("SDL",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    WIDTH,
-    HEIGHT,
-    SDL_WINDOW_SHOWN);
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WIDTH,
+		HEIGHT,
+		SDL_WINDOW_RESIZABLE);
 
 	if (pWindow == NULL)
 	{
 		fprintf(stdout, "%s\n", SDL_GetError());
-        free(array);
-        SDL_Quit();
-        exit(EXIT_FAILURE);
-	}
-    
-    rest();
-    free(array);
-    SDL_DestroyWindow(pWindow);
-	SDL_Quit();
-	
-    
-    /*SDL_Init(SDL_INIT_EVERYTHING);
-
-    screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-	
-
-	if (screen == NULL)
-	{
-		fprintf(stdout, "%s\n", SDL_GetError());
+		SDL_Quit();
 		exit(EXIT_FAILURE);
 	}
 
+	pRenderer = SDL_CreateRenderer(
+		pWindow,
+		DEFAULT_INDEX,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	if (pRenderer == NULL)
+	{
+		fprintf(stdout, "%s\n", SDL_GetError());
+		SDL_DestroyWindow(pWindow);
+		SDL_Quit();
+		exit(EXIT_FAILURE);
+	}
+
+	SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
+	SDL_RenderClear(pRenderer);
+
+	SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 20);
+	SDL_RenderPresent(pRenderer);
+
+
+	draw(node, &bound);
+
+
 	rest();
 
-	SDL_FreeSurface(screen);
-	SDL_Quit();*/
-    
-    
+
+	SDL_DestroyRenderer(pRenderer);
+	SDL_DestroyWindow(pWindow);
+	SDL_Quit();
+
+	Root_Free(node);*/
+
 	exit(EXIT_SUCCESS);
 }
 
+int is_mtrFile(char *file_name)
+{
+	int size = (int)(strlen(file_name) >= 5) ? strlen(file_name) - EXT_OSM_LENGTH : 0;
+
+	if (!strncmp(".osm", &file_name[size], EXT_OSM_LENGTH))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
