e34102b5f12f239ea492264df4a1ae5d6bd3a0c9
yazidlefoot <yazid_lefoot@hotmail.fr>
2016-03-09 13:28:22 +0100

    Update errs.h

--- errs.h
+++ errs.h
@@ -1,8 +1,13 @@
 #ifndef ERRS_INCLUDED_H
 #define ERRS_INCLUDED_H
 
-#define READ_XML_FAILURE	"Internal error reading a xml file"
-#define PARSING_FAILURE		"Internal error parsing a xml file"
+#define NUMBER_ARGUMENT_FAILURE		"./main <file_name.osm>"
+#define READ_XML_FAILURE			"Internal error reading a xml file"
+#define PARSING_FAILURE				"Internal error parsing a xml file"
+#define NOT_AN_OS_FILE				"The file given is not an osm file"
 
-
+#define PARSE_FILE_FAILURE 			"Error xmlParseFile"
+#define GET_ROOT_ELEMENT_FAILURE	"Error xmlDocGetRootElement"
+#define GET_SIZE_AND_BOUND 			"Error GetSizeAndBound"
+#define ROOT_GET_FAILURE			"Error Root_Get"
 #endif
