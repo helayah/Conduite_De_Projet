24850b278cd17245809d0129db4f69a6ea40d802
yazidlefoot <yazid_lefoot@hotmail.fr>
2016-03-29 22:49:01 +0200

    Update objs.h

--- objs.h
+++ objs.h
@@ -3,7 +3,6 @@
 
 #include "libs.h"
 
-<<<<<<< HEAD
 #define UNKNOW 		-1
 #define BUILDING 	(1 << 0)
 #define HIGHWAY 	(1 << 1)
@@ -13,71 +12,48 @@
 #define RAILWAY 	(1 << 6)
 #define BRIDGE 		(1 << 7)
 
-=======
->>>>>>> origin/master
 typedef unsigned int type_t;
 
-typedef struct position {
-	float latitude;
-	float longitude;
-} position;
-
-typedef struct user {
-	char* name;
-	uint16_t id;
-} user;
-
-typedef struct ways {
-	uint32_t id;
-	int size;
-	uint32_t* ref;
-	type_t type;
-} ways;
-
-typedef struct bounds {
+typedef struct bound {
 	double minlat;
 	double minlon;
 	double maxlat;
 	double maxlon;
-} bounds;
+} bound;
+
+typedef struct way {
+	uint32_t id;
+	int size;
+	uint32_t* ref;
+	type_t type;
+} way;
 
 typedef struct node_t {
 	uint32_t id;
 	double latitude;
 	double longitude;
-	/*position* pos;
-	struct user puser;
-	int visible;
-	unsigned short version;
-	uint32_t changeset;
-	time_t timestamp;*/
 } node_t;
 
-typedef struct root {
-	int size_node;
-	int size_way;
-	bounds* dimension;
-	node_t** arrayNode;
-	ways** arrayWays;
-} root;
+typedef struct Root {
+	int size_nodes;
+	int size_ways;
+	node_t** arrayNodes;
+	way** arrayWays;
+} Root;
 
-void Root_Free(root* node);
-root* Root_AddNode(root* r, node_t* node);
-root* Root_AddWay(root* r, ways* way);
-root* Root_Init(int size_node, int size_way, bounds* b);
+int Bound_Init(double minlat, double maxlat, double minlon, double maxlon);
+int Root_Init(int sn, int sw);
+
 node_t* Node_Init(uint32_t id, double lat, double lon);
-ways* Ways_Init(uint32_t id, int size);
-<<<<<<< HEAD
-node_t* Root_GetNode(root* node, int length, uint32_t reference);
+way* Ways_Init(uint32_t id, int size);
 
-void Root_Display(root* node);
-void Way_Display(ways* way);
-void Ref_Display(uint32_t* ref, int size);
-=======
-node_t* getNode(node_t** node, int length, uint32_t reference);
-
-void Root_Display(root* node);
->>>>>>> origin/master
+void Bound_Display();
+void Root_Display();
 void Node_Display(node_t* node);
-void Bound_Display(bounds* b);
+void Way_Display(way* way);
+void Ref_Display(uint32_t* ref, int size);
+
+void Root_Free();
+void Objs_Free();
 #endif
+
