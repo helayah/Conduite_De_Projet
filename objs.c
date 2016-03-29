03693408e69b14701d7dfbfa6b32aee3518d5dc5
yazidlefoot <yazid_lefoot@hotmail.fr>
2016-03-29 22:54:17 +0200

    Update objs.c

--- objs.c
+++ objs.c
@@ -1,151 +1,70 @@
 #include "objs.h"
 
-void Root_Free(root* node)
+extern Root* pRoot;
+extern bound* pBound;
+
+int Bound_Init(double minlat, double minlon, double maxlat, double maxlon)
 {
-	int i;
-
-	if (node->arrayNode != NULL)
+	if ((pBound = malloc(sizeof(*pBound))) == NULL)
 	{
-		for (i = 0; i < node->size_node; i++)
-		{
-			if (node->arrayNode[i] != NULL)
-				free(node->arrayNode[i]);
-		}
-		free(node->arrayNode);
+		Objs_Free();
+		return FAILURE;
 	}
-
-	if (node->arrayWays != NULL)
-	{
-		for (i = 0; i < node->size_way; i++)
-		{
-			if (node->arrayWays[i] != NULL)
-			{
-				free(node->arrayWays[i]);
-			}
-
-		}
-		free(node->arrayWays);
-	}
-
-	free(node->dimension);
-	free(node);
+	
+	pBound->minlat = minlat;
+	pBound->minlon = minlon;
+	pBound->maxlat = maxlat;
+	pBound->maxlon = maxlon;
+	
+	return SUCCESS;
 }
 
-root* Root_AddNode(root* r, node_t* node)
+int Root_Init(int sn, int sw)
 {
 	int i;
 	
-	for(i = 0; i < r->size_node; i++)
-	{
-		if(r->arrayNode[i] == NULL)
-			r->arrayNode[i] = node;	
-	}
-	
-	return r;
-}
+	if (sn < 0 || sn >(size_t) - 1 / sizeof(pRoot->arrayNodes[0]))
+		return FAILURE;
 
-root* Root_AddWay(root* r, ways* way)
-{
-	int i;
-	
-	for(i = 0; i < r->size_way; i++)
-	{
-		if(r->arrayWays[i] == NULL)
-			r->arrayWays[i] = way;	
-	}
-	
-	return r;
-}
-
-root* Root_Init(int size_node, int size_way, bounds* b)
-{
-	root* node;
-	int i;
-	
-	if (size_node < 0 || size_node >(size_t) - 1 / sizeof(node->arrayNode[0]))
-		return NULL;
-
-	if (size_way < 0 || size_way >(size_t) - 1 / sizeof(node->arrayWays[0]))
-		return NULL;
-	
-	if(b == NULL)
-		return NULL;
-
-	if ((node = malloc(sizeof(*node))) == NULL)
-		return NULL;
+	if (sw < 0 || sw >(size_t) - 1 / sizeof(pRoot->arrayWays[0]))
+		return FAILURE;
 		
-	if ((node->dimension = malloc(sizeof(*node->dimension))) == NULL)
+	if ((pRoot = malloc(sizeof(*pRoot))) == NULL)
 	{
-		free(node);
-		return NULL;
+		free(pBound);
+		return FAILURE;
 	}
-
-	node->size_node = size_node;
-	node->size_way = size_way;
-	node->dimension->minlat = b->minlat;
-	node->dimension->maxlat = b->maxlat;
-	node->dimension->minlon = b->minlon;
-	node->dimension->maxlon = b->maxlon;
 	
-	node->arrayNode = malloc(size_node * sizeof(*node->arrayNode[0]));
+	pRoot->size_nodes = sn;
+	pRoot->size_ways = sw;
+	pRoot->arrayNodes = malloc(pRoot->size_nodes * sizeof(*pRoot->arrayNodes[0]));
 
-	if (node->arrayNode == NULL)
+	if (pRoot->arrayNodes == NULL)
 	{
-		free(node->dimension);
-		free(node);
-		return NULL;
+		Objs_Free();
+		return FAILURE;
 	}
 
-	for (i = 0; i < size_node; i++)
-		node->arrayNode[i] = NULL;
+	for (i = 0; i < pRoot->size_nodes; i++)
+		pRoot->arrayNodes[i] = NULL;
 
-	node->arrayWays = malloc(size_way * sizeof(*node->arrayWays[0]));
+	pRoot->arrayWays = malloc(pRoot->size_ways * sizeof(*pRoot->arrayWays[0]));
 
-	if (node->arrayWays == NULL)
+	if (pRoot->arrayWays == NULL)
 	{
-		free(node->dimension);
-		free(node->arrayNode);
-		free(node);
-		return NULL;
+		Objs_Free();
+		return FAILURE;
 	}
 
-	for (i = 0; i < size_way; i++)
-		node->arrayWays[i] = NULL;
-
-	return node;
-}
-
-ways* Ways_Init(uint32_t id, int size)
-{
-	ways* way;
-	int i;
-
-	if (size < 0 || size >(size_t) - 1 / sizeof(way->ref))
-		return NULL;
-
-	if ((way = malloc(sizeof(*way))) == NULL)
-		return NULL;
-
-	way->id = id;
-	way->size = size;
-	way->type = UNKNOW;
-	way->ref = malloc(size * sizeof(*way->ref));
-
-	if (way->ref == NULL)
-	{
-		free(way);
-		return NULL;
-	}
-
-	for (i = 0; i < size; i++)
-		way->ref[i] = 0;
-
-	return way;
+	for (i = 0; i < pRoot->size_ways; i++)
+		pRoot->arrayWays[i] = NULL;
+	
+	return SUCCESS;
 }
 
 node_t* Node_Init(uint32_t id, double lat, double lon)
 {
-	node_t* node;
+	node_t* node = NULL;
 
 	if ((node = malloc(sizeof(*node))) == NULL)
 		return NULL;
@@ -157,42 +76,87 @@ node_t* Node_Init(uint32_t id, double lat, double lon)
 	return node;
 }
 
-node_t* Root_GetNode(root* node, int length, uint32_t reference)
+way* Ways_Init(uint32_t id, int size)
 {
+	way* w = NULL;
 	int i;
 
-	for (i = 0; i < length; i++)
+	if (size < 0 || size >(size_t) - 1 / sizeof(w->ref))
+		return NULL;
+
+	if ((w = malloc(sizeof(*w))) == NULL)
+		return NULL;
+
+	w->id = id;
+	w->size = size;
+	w->type = UNKNOW;
+	w->ref = malloc(size * sizeof(*w->ref));
+
+	if (w->ref == NULL)
 	{
+		free(w);
+		return NULL;
 	}
 
-	return NULL;
+	for (i = 0; i < size; i++)
+		w->ref[i] = 0;
+
+	return w;
 }
 
+/*int Root_Add(node_t n, int index)
+{
+	if(n == NULL)
+		return FAILURE;
 
-void Root_Display(root* node)
+
+}*/
+
+void Bound_Display()
+{
+	printf("----------- Bounds -----------\n");
+	printf("minlat	= %6.7f\n", pBound->minlat);
+	printf("minlon	= %6.7f\n", pBound->minlon);
+	printf("maxlat	= %6.7f\n", pBound->maxlat);
+	printf("maxlon 	= %6.7f\n", pBound->maxlon);
+	printf("------------------------------\n");
+}
+
+void Root_Display()
 {
 	int i;
 
-	//printf("size_node = %d\n", node->size_node);
-	//printf("size_way = %d\n", node->size_way);
-	for (i = 0; i < node->size_node; i++)
+	printf("size_nodes = %d\n", pRoot->size_nodes);
+	printf("size_ways = %d\n", pRoot->size_ways);
+	for (i = 0; i < pRoot->size_nodes; i++)
 	{
-		if (node->arrayNode[i] != NULL)
-			Node_Display(node->arrayNode[i]);
+		if(i == 20)
+			break;
+		/*if (pRoot->arrayNodes[i] != NULL)
+			Node_Display(pRoot->arrayNodes[i]);*/
 			
-		/*if (node->arrayWays[i] != NULL)
-			Way_Display(node->arrayWays[i]);*/
+		if (pRoot->arrayWays[i] != NULL)
+			Way_Display(pRoot->arrayWays[i]);
 
 	}
 }
 
-void Way_Display(ways* way)
+void Node_Display(node_t* node)
+{
+	printf("------------- Node -------------\n");
+	printf("id 		= %" PRId32 "\n", node->id);
+	printf("latitude 	= %6.7f\n", node->latitude);
+	printf("longitude 	= %6.7f\n", node->longitude);
+	printf("--------------------------------\n");
+}
+
+void Way_Display(way* w)
 {
 	printf("------------- Way -------------\n");
-	printf("id 	= %" PRId32 "\n", way->id);
-	printf("size 	= %d\n", way->size);
-	printf("type 	= %d\n", way->type);
-	Ref_Display(way->ref, way->size);
+	printf("id 	= %" PRId32 "\n", w->id);
+	printf("size_ref = %d\n", w->size);
+	printf("type 	= %d\n", w->type);
+	Ref_Display(w->ref, w->size);
 	printf("-------------------------------\n");
 }
 
@@ -206,21 +170,40 @@ void Ref_Display(uint32_t* ref, int size)
 	}
 }
 
-void Node_Display(node_t* node)
+void Root_Free()
 {
-	printf("------------- Node -------------\n");
-	printf("id 		= %" PRId32 "\n", node->id);
-	printf("latitude 	= %6.7f\n", node->latitude);
-	printf("longitude 	= %6.7f\n", node->longitude);
-	printf("--------------------------------\n");
+	int i;
+
+	if (pRoot->arrayNodes != NULL)
+	{
+		for (i = 0; i < pRoot->size_nodes; i++)
+		{
+			if (pRoot->arrayNodes[i] != NULL)
+				free(pRoot->arrayNodes[i]);
+		}
+		free(pRoot->arrayNodes);
+	}
+
+	if (pRoot->arrayWays != NULL)
+	{
+		for (i = 0; i < pRoot->size_ways; i++)
+		{
+			if (pRoot->arrayWays[i] != NULL)
+			{
+				free(pRoot->arrayWays[i]);
+			}
+
+		}
+		free(pRoot->arrayWays);
+	}
+
+	free(pRoot);
 }
 
-void Bound_Display(bounds* b)
+void Objs_Free()
 {
-	printf("----------- Bounds -----------\n");
-	printf("minlat	= %6.7f\n", b->minlat);
-	printf("minlon	= %6.7f\n", b->minlon);
-	printf("maxlat	= %6.7f\n", b->maxlat);
-	printf("maxlon 	= %6.7f\n", b->maxlon);
-	printf("------------------------------\n");
+	if(pRoot != NULL)
+		Root_Free();
+	if(pBound != NULL)
+		free(pBound);
 }
