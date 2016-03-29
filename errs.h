7f6b08505013f151d027496b9d79c80f27e058e9
yazidlefoot <yazid_lefoot@hotmail.fr>
2016-03-29 22:45:28 +0200

    Update errs.h

--- errs.h
+++ errs.h
@@ -1,13 +1,7 @@
 #ifndef ERRS_INCLUDED_H
 #define ERRS_INCLUDED_H
 
-#define NUMBER_ARGUMENT_FAILURE		"./main <file_name.osm>"
-#define READ_XML_FAILURE			"Internal error reading a xml file"
-#define PARSING_FAILURE				"Internal error parsing a xml file"
-#define NOT_AN_OS_FILE				"The file given is not an osm file"
+#define ARGC_FAILURE "Nombre d'arguments invalide"
 
-#define PARSE_FILE_FAILURE 			"Error xmlParseFile"
-#define GET_ROOT_ELEMENT_FAILURE	"Error xmlDocGetRootElement"
-#define GET_SIZE_AND_BOUND 			"Error GetSizeAndBound"
-#define ROOT_GET_FAILURE			"Error Root_Get"
 #endif
+
